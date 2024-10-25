622c42dae7609dbe1e5f854fa7269689c1e4bfe1
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2024-10-25 10:06:22 +0600

    added navbar and start and middle col

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import Home from './pages/Home.vue';
 import Login from './pages/Login.vue';
 </script>
 
 <template>
-  <div class="absolute -z-50 h-screen w-screen bg-black"></div>
-  <Login />
+  <div class="absolute hidden -z-50 h-screen w-screen bg-black" id="backdrop"></div>
+  <Home />
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import 'material-symbols'
 import './style.css'
 import App from './App.vue'
 
